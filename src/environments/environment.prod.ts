d3f94c3ef78ad56ee4208f27e89ad2933bf1b3c9
Ihor Tsoi <ihor.tsoi@nure.ua>
2020-11-26 01:17:58 +0200

    Environment copied.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,25 @@
+const BASE_API_URL = 'https://localhost:5001';
+
 export const environment = {
-  production: true
+  production: true,
+  baseAPIUrl: BASE_API_URL,
+  endpoints: {
+    loginAdmin: BASE_API_URL + '/admins/login',
+    loginCustomer: BASE_API_URL + '/customers/login',
+    registerCustomer: BASE_API_URL + '/customers/register',
+    getCustomerPaymentAmount: BASE_API_URL + '/customers/payment',
+    payCustomer: BASE_API_URL + '/customers/payment',
+    registerAdmin: BASE_API_URL + '/admins/register',
+    registerSmartDevice: BASE_API_URL + '/smartdevices/register',
+    export: BASE_API_URL + '/servicedata',
+    import: BASE_API_URL + '/servicedata',
+    getAllSmartDevices: BASE_API_URL + '/smartdevices',
+    blockSmartDevice: BASE_API_URL + '/smartdevices/{id}/block',
+    resetSmartDevice: BASE_API_URL + '/smartdevices/{id}/reset',
+    getStatisticsForSmartDevice: BASE_API_URL + '/smartdevices/{id}/views',
+    getCustomerAdvertisements: BASE_API_URL + '/advertisements',
+    changeStatusOfAdvertisement: BASE_API_URL + '/advertisements/{id}',
+    getStatisticsForAdvertisement: BASE_API_URL + '/advertisements/{id}/views',
+    createAdvertisement: BASE_API_URL + '/advertisements'
+  }
 };
